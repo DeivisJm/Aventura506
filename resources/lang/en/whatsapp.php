954eaacf397fbf9a350784d62ac2f4e531fc4f65
Deivis Jimenez <deivisjm1205@gmail.com>
2026-04-01 18:22:56 -0600

    Add multilingual WhatsApp floating button message based on current site locale

---
+++ resources/lang/en/whatsapp.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    // Floating WhatsApp button
+    'floating_message' => 'Hello Aventura506, I would like to receive information about tours and accommodations in La Fortuna.',
+
+];
---
+++ resources/lang/es/whatsapp.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    // Floating WhatsApp button
+    'floating_message' => 'Hola Aventura506, me gustaría recibir información sobre tours y hospedaje en La Fortuna.',
+
+];
